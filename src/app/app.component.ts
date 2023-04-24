0650b1374ec0fe3fa638ec7e97e2928c8e2602fa
Andre Luiz <adventureandre@hotmail.com>
2023-04-24 20:23:17 -0300

    Estudos angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template : `
-    <app-data-binding></app-data-binding>
+<app-diretivas-estruturais></app-diretivas-estruturais>
+    <!--    <app-data-binding></app-data-binding>-->
     <router-outlet></router-outlet>`
 })
 export class AppComponent{
