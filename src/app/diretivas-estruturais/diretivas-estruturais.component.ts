e0a5206788d764b8aa68d47f66aa5504c44b8e7f
Andre Luiz <adventureandre@hotmail.com>
2023-04-24 21:58:46 -0300

    Estudos angular ngFor

---
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-estruturais',
+  templateUrl: './diretivas-estruturais.component.html',
+  styleUrls: ['./diretivas-estruturais.component.scss']
+})
+export class DiretivasEstruturaisComponent implements OnInit {
+
+  public condicao: boolean = true
+  public condicaoClick: boolean = true
+
+  ngOnInit() {
+    setInterval((): any => {
+      this.condicao = !this.condicao;
+
+    }, 1000)
+  }
+
+  public onClick() {
+    this.condicaoClick = !this.condicaoClick;
+  }
+
+
+  public list: Array<{ nome: string, idade:number }> = [
+    {nome: "Andre Luiz", idade:33},
+    {nome:'Milena Camilla', idade:22},
+    {nome:'Eunice',idade:48},
+    {nome:'Sivaldo',idade:53}
+  ]
+
+  public onClickAddList(){
+    this.list.push({nome:"Nay",idade:31})
+  }
+}
