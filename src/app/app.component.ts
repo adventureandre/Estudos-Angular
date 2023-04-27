e0aac19914d1d7b14e3905857b18d1bb057ccbc3
Andre Luiz <adventureandre@hotmail.com>
2023-04-27 12:29:28 -0300

    Estudos Angular Pipes E Date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template : `
-      <app-diretivas-atributos>
-        <h1>Aulas de diretivas de Atributos</h1>
-        <hr>
-      </app-diretivas-atributos>
-<!--      <app-diretivas-estruturais></app-diretivas-estruturais>-->
+    <app-diretivas-atributos></app-diretivas-atributos>
+      <!--      <app-diretivas-atributos>-->
+      <!--        <h1>Aulas de diretivas de Atributos</h1>-->
+      <!--        <hr>-->
+      <!--      </app-diretivas-atributos>-->
+      <!--      <app-diretivas-estruturais></app-diretivas-estruturais>-->
       <!--    <app-data-binding></app-data-binding>-->
       <router-outlet></router-outlet>`
 })
--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -1,38 +1,40 @@
 import {Component, OnInit} from '@angular/core';
+import {dateTimestampProvider} from "rxjs/internal/scheduler/dateTimestampProvider";
 
 @Component({
   selector: 'app-diretivas-atributos',
   templateUrl: './diretivas-atributos.component.html',
   styleUrls: ['./diretivas-atributos.component.scss']
 })
-export class DiretivasAtributosComponent implements OnInit{
+export class DiretivasAtributosComponent implements OnInit {
 
-  public valor :boolean = true
-  public height:string = '20px';
-  backgroundColor:string =  'red'
+  public valor: boolean = true
+  public height: string = '20px';
+  backgroundColor: string = 'red'
 
-  public nome:string = ""
-  public list:Array<{nome:string}> = [{nome: 'andre'}]
+  public date:Date =  new Date();
+  public nome: string = ""
+  public list: Array<{ nome: string }> = [{nome: 'andre'}]
 
   ngOnInit() {
-    setInterval(()=>{
+    setInterval(() => {
       this.valor = !this.valor
 
-      if(this.height === '20px'){
+      if (this.height === '20px') {
         this.height = '50px'
         this.backgroundColor = 'blue'
-      }else{
+      } else {
         this.height = '20px'
       }
-    },2000)
+    }, 2000)
   }
 
-  public salvar(){
-      this.list.push({nome:this.nome})
-      this.nome = ''
+  public salvar() {
+    this.list.push({nome: this.nome})
+    this.nome = ''
   }
 
-  public mudaValor(){
+  public mudaValor() {
     this.valor = !this.valor
   }
 
